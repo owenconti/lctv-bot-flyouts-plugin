d8dd36bed4e1cbf4ef1e4c0fd4a1b324770da302
Owen Conti <owen@ohseemedia.com>
2015-11-05 23:02:48 -0700

    Cleaned up the client.js code, and added flyout queues. Added instructions for setup to the readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # lctv-bot-flyouts-plugin
 Flyouts plugin for the LCTV Bot
+
+## Setup
+To add flyouts on command, add a `settings.json` file in the plugin folder with the following structure:
+
+```
+{
+    "flyouts" : [{
+        // Anytime '#doge' is said in chat,
+        // the doge.png will flyout with the flyLeft animation
+        "command" : "#doge",
+        "image" : "doge.png",
+        "animation" : "flyLeft"
+    }, {
+        // Anytime '#test' is said in chat,
+        // the doge.png will flyout with the flyBottom animation
+        "command" : "#test",
+        "image" : "doge.png",
+        "animation" : "flyBottom"
+    }, {
+        // Anytime '#scale' is said in chat,
+        // the doge.png will flyout with the scale animation
+        "command" : "#scale",
+        "image" : "doge.png",
+        "animation" : "scale",
+        "template" : "{{username}} followed the stream"
+    }, {
+        // Anytime '#fade' is said in chat,
+        // the doge.png will flyout with the fade animation
+        "command" : "#fade",
+        "image" : "doge.png",
+        "animation" : "fade",
+        "template" : "Thanks for the follow!"
+    }]
+}
+```
--- client.js
+++ client.js
@@ -18,188 +18,218 @@ module.exports = {
             height: '700px'
         });
 
+        var animationQueue = [];
+        var animationInProgress = false;
+
 		registerSocketMessage( 'flyout', function( messageObj ) {
-			var $div = $('div#flyout');
-            var duration = messageObj.duration || 5000;
-
-            if ( messageObj.type === 'image' ) {
-                var $content = $( '<img />', {
-                    src: 'data:image/png;base64,' + messageObj.content
+            // Check if the request animation is valid
+            var requestedAnimation = animations[ messageObj.animation ];
+            if ( !requestedAnimation ) {
+                // If the animation doesn't exist,
+                // don't show the image.
+                 $('div#flyout').css({
+                    left: 'auto',
+                    right: 'auto',
+                    top: 'auto',
+                    bottom: 'auto',
+                    opacity: 0
                 });
-            } else if ( messageObj.type === 'div' ) {
-                var $content = $( messageObj.content );
-            }
-            $div.html( $content );
 
-            switch( messageObj.animation ) {
-                case 'fade':
-                    fade( $div, duration );
-                    break;
-                case 'scale':
-                    scale( $div, duration );
-                    break;
-                case 'fly-bottom':
-                    flyBottom( $div, duration );
-                    break;
-                case 'fly-left':
-                    flyLeft( $div, duration );
-                    break;
-                case 'fly-top':
-                    flyTop( $div, duration );
-                    break;
-                case 'fly-right':
-                    flyRight( $div, duration );
-                    break;
-                default:
-                    // If the animation doesn't exist,
-                    // don't show the image.
-                    $div.css({
-                        left: 'auto',
-                        right: 'auto',
-                        top: 'auto',
-                        bottom: 'auto',
-                        opacity: 0
-                    });
-                    break;
+                console.log( 'Invalid animation request: ' + messageObj.animation );
+                return;
+            }
+
+            // Add the animation to the queue and run the queue
+            animationQueue.push( {
+                messageObj: messageObj,
+                animationFunction: requestedAnimation
+            } );
+            runAnimationQueue();
+
+            /**
+             * Runs the animation queue.
+             * Starts animating the first function in the queue.
+             * Removes the animated function from the queue once ran.
+             * @return {void}
+             */
+            function runAnimationQueue() {
+                if ( animationQueue.length === 0 ) {
+                    return;
+                }
+
+                // If no animation is running,
+                // run the first in the queue
+                if ( !animationInProgress ) {
+                    var animationObj = animationQueue.shift();
+                    var messageObj = animationObj.messageObj;
+                    var animationFunction = animationObj.animationFunction;
+                    var duration = messageObj.duration || 5000;
+
+                    // Build the div to flyout
+                    var $div = $('div#flyout');
+                    var $content = null;
+                    if ( messageObj.type === 'image' ) {
+                        $content = $( '<img />', {
+                            src: 'data:image/png;base64,' + messageObj.content
+                        });
+                    } else if ( messageObj.type === 'div' ) {
+                        $content = $( messageObj.content );
+                    }
+                    $div.html( $content );
+
+                    // Run the animation
+                    animationInProgress = true;
+                    animationFunction( $div, duration, handleAnimationComplete );
+                }
+            }
+
+            /**
+             * Handles when an animation finishes.
+             * @return {void}
+             */
+            function handleAnimationComplete() {
+                animationInProgress = false;
+                runAnimationQueue();
             }
 		} );
 
         /**
-         * Fades the image in
-         * @param  {jQuery object} $div
-         * @return {void}
+         * Available animations.
+         * @type {Object}
          */
-        function fade( $div, duration ) {
-            $div.css({
-                left: 50,
-                right: 'auto',
-                top: 'auto',
-                bottom: 50,
-                opacity: 0
-            });
-            $div.velocity( {
-                opacity: 1
-            } );
-            $div.velocity( "reverse", {
-                delay: duration
-            } );
-        }
+        var animations = {
+            /**
+             * Fades the image in
+             */
+            fade: function( $div, duration, callback ) {
+                $div.css({
+                    left: 50,
+                    right: 'auto',
+                    top: 'auto',
+                    bottom: 50,
+                    opacity: 0
+                });
+                $div.velocity( {
+                    opacity: 1
+                } );
+                $div.velocity( "reverse", {
+                    delay: duration,
+                    complete: callback
+                } );
+            },
 
-        /**
-         * Scales the image from 0% to 100%
-         * @param  {jQuery object} $div
-         * @return {void}
-         */
-        function scale( $div, duration ) {
-            $div.css({
-                left: 50,
-                right: 'auto',
-                top: 'auto',
-                bottom: 50,
-                opacity: 1
-            });
-            $div.velocity({
-                scale: 0
-            }, {
-                duration: 1
-            }).velocity( {
-                scale: 1,
-            } );
+            /**
+             * Scales the image from 0% to 100%
+             */
+            scale: function( $div, duration, callback ) {
+                $div.css({
+                    left: 50,
+                    right: 'auto',
+                    top: 'auto',
+                    bottom: 50,
+                    opacity: 0
+                });
+                $div.velocity({
+                    scale: 0,
+                    opacity: 1
+                }, {
+                    duration: 1
+                }).velocity( {
+                    scale: 1,
+                } );
 
-            $div.velocity( {
-                scale: 0
-            }, {
-                delay: duration
-            } ).velocity({
-                scale: 1,
-                opacity: 0
-            }, {
-                duration: 1
-            });
-        }
+                $div.velocity( {
+                    scale: 0
+                }, {
+                    delay: duration
+                }).velocity({
+                    scale: 1,
+                    opacity: 0
+                }, {
+                    duration: 1,
+                    complete: callback
+                });
+            },
 
-        /**
-         * Fly in the image from the left
-         * @param  {jQuery object} $div
-         * @return {void}
-         */
-        function flyLeft( $div, duration ) {
-            $div.css({
-                left: '-150%',
-                right: 'auto',
-                top: 'auto',
-                bottom: 50,
-                opacity: 1
-            });
-            $div.velocity( {
-                left: 50
-            } );
-            $div.velocity( "reverse", {
-                delay: duration
-            } );
-        }
+            /**
+             * Fly in the image from the left
+             */
+            flyLeft: function( $div, duration, callback ) {
+                $div.css({
+                    left: '-150%',
+                    right: 'auto',
+                    top: 'auto',
+                    bottom: 50,
+                    opacity: 1
+                });
+                $div.velocity( {
+                    left: '50px'
+                } );
+                $div.velocity( "reverse", {
+                    delay: duration,
+                    complete: callback
+                } );
+            },
 
-        /**
-         * Fly in the image from the right
-         * @param  {jQuery object} $div
-         * @return {void}
-         */
-        function flyRight( $div, duration ) {
-            $div.css({
-                right: '-100%',
-                bottom: 50,
-                top: 'auto',
-                left: 'auto',
-                opacity: 1
-            });
-            $div.velocity( {
-                right: 50
-            } );
-            $div.velocity( "reverse", {
-                delay: duration
-            } );
-        }
+            /**
+             * Fly in the image from the right
+             */
+            flyRight: function( $div, duration, callback ) {
+                $div.css({
+                    right: '-100%',
+                    bottom: 50,
+                    top: 'auto',
+                    left: 'auto',
+                    opacity: 1
+                });
+                $div.velocity( {
+                    right: 50
+                } );
+                $div.velocity( "reverse", {
+                    delay: duration,
+                    complete: callback
+                } );
+            },
 
-        /**
-         * Fly in the image from the top
-         * @param  {jQuery object} $div
-         * @return {void}
-         */
-        function flyTop( $div, duration ) {
-            $div.css({
-                top: '-100%',
-                right: 'auto',
-                bottom: 'auto',
-                left: 50,
-                opacity: 1
-            });
-            $div.velocity( {
-                top: '0'
-            } );
-            $div.velocity( "reverse", {
-                delay: duration
-            } );
-        }
+            /**
+             * Fly in the image from the top
+             */
+            flyTop: function( $div, duration, callback ) {
+                $div.css({
+                    top: '-100%',
+                    right: 'auto',
+                    bottom: 'auto',
+                    left: 50,
+                    opacity: 1
+                });
+                $div.velocity( {
+                    top: '0'
+                } );
+                $div.velocity( "reverse", {
+                    delay: duration,
+                    complete: callback
+                } );
+            },
 
-        /**
-         * Fly in from the bottom
-         * @param  {jQuery Object} $div
-         * @return {void}
-         */
-        function flyBottom( $div, duration ) {
-            $div.css({
-                bottom: '-150%',
-                right: 'auto',
-                top: 'auto',
-                left: 50,
-                opacity: 1
-            });
-            $div.velocity( {
-                bottom: 50
-            } );
-            $div.velocity( "reverse", {
-                delay: duration
-            } );
-        }
+            /**
+             * Fly in from the bottom
+             */
+            flyBottom: function( $div, duration, callback ) {
+                $div.css({
+                    bottom: '-150%',
+                    right: 'auto',
+                    top: 'auto',
+                    left: 50,
+                    opacity: 1
+                });
+                $div.velocity( {
+                    bottom: 50
+                } );
+                $div.velocity( "reverse", {
+                    delay: duration,
+                    complete: callback
+                } );
+            }
+        };
 	}
 };
